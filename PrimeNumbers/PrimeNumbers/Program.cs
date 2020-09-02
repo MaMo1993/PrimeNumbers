32357503cea853c5b7b75a2721f5ea357ae0a9ae
Morongoa Mamabolo <60958581+MaMo1993@users.noreply.github.com>
2020-09-02 21:22:43 +0200

    Add files via upload

---
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mod = 0, numberofInts = 0, primeInt = 1, anyInt = 1;
+            Console.Write("Firt 100 Prime Numbers:" + Environment.NewLine) ;
+            while (numberofInts < 100)
+            {
+                anyInt = 1;
+                mod = 0;
+                while (anyInt <= primeInt)
+                {
+                    if (primeInt % anyInt == 0)
+                        mod++;
+                    anyInt++;
+                }
+
+                if (mod == 2)
+                {
+                    Console.Write(primeInt);
+                    Console.Write(" " + Environment.NewLine);
+                    numberofInts++;
+                }
+                primeInt++;
+            }
+        }
+    }
+}
